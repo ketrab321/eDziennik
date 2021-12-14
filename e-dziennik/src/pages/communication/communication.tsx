047d3582a940f16815c371c42c0d6862f5dc2628
ketrab321 <ketrab54321@gmail.com>
2021-12-15 00:18:45 +0100

    Connect firebase

--- e-dziennik/src/pages/communication/communication.tsx
+++ e-dziennik/src/pages/communication/communication.tsx
@@ -1,5 +1,10 @@
 import { Box } from "@mui/system";
+import CommunicationPanel from "./communicationPanel";
 
-export default function Communication() {
-  return <Box>Wiadomości</Box>;
+export default function Communication(props: { firebaseApp: any }) {
+  return (
+    <Box>
+      <CommunicationPanel firebaseApp={props.firebaseApp} />
+    </Box>
+  );
 }
